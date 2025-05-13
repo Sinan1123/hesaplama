e9ceebd10741225adf1c80f63c3ad114dc5d6a20
sinan gürses <95588874+Sinan1123@users.noreply.github.com>
2025-05-13 04:48:57 +0300

    Update Program.cs

--- hesaplama/Program.cs
+++ hesaplama/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace hesaplama
+namespace hesaplama
 {
     internal class Program
     {
@@ -8,6 +8,8 @@
             {
                 Console.WriteLine("Hesaplama uygulamasına hoş geldiniz! \nToplama için 1, çıkarma için 2, çarpma için 3, bölme için 4, çıkmak için 0 a basın");
                 int choose;
+                Console.Write("Seçiminiz: ");
+
                 if (!int.TryParse(Console.ReadLine(), out choose))
                 {
                     Console.WriteLine("Gerçersiz bir işlem yaptınız");
@@ -23,6 +25,12 @@
                     Console.Write("\rÇıkılıyor...");
                     Thread.Sleep(500);
                     Console.Write("\rProgramdan çıkıldı...\n");
+                    break;
+                }
+                if (choose < 1 || choose > 4)
+                {
+                    Console.WriteLine("Geçersiz seçim! 1-4 arasında bir sayı giriniz.");
+                    continue; 
                 }
                 Console.Write("Birinci sayıyı giriniz: ");
                 int number1 = Convert.ToInt32(Console.ReadLine());
@@ -31,16 +39,17 @@
                 int number2 = Convert.ToInt32(Console.ReadLine());
                 int result = 0;
                 bool isAcceptable = true;
+
                 switch (choose)
                 {
                     case 1:
-                        result = Toplama(number1, number2);
+                        result = Plus(number1, number2);
                         break;
                     case 2:
-                        result = Cikarma(number1, number2);
+                        result = Minus(number1, number2);
                         break;
                     case 3:
-                        result = Carpma(number1, number2);
+                        result = Multiply(number1, number2);
                         break;
                     case 4:
                         if (number2 == 0)
@@ -58,15 +67,17 @@
                         isAcceptable = false;
                         break;
                 }
-                if (isAcceptable)
+                if (isAcceptable == true)
+                {
                     Console.WriteLine("İşleminizin sonucu: " + result);
+                }
             }
         }   
-        static int Toplama(int a, int b)
+        static int Plus(int a, int b)
         { return a + b; }
-        static int Cikarma(int a, int b)
+        static int Minus(int a, int b)
         { return a - b; }
-        static int Carpma(int a, int b)
+        static int Multiply(int a, int b)
         { return a * b; }
         static int Divide(int a, int b)
         { return a / b; }
